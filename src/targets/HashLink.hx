3bb9b5d71ed69f876a4d9ca25f678250d17fb2c2
Casey Clyde <dev@Caseys-Air.attlocal.net>
2023-01-30 13:40:25 -0800

    work on mac build

--- src/targets/HashLink.hx
+++ src/targets/HashLink.hx
@@ -15,71 +15,74 @@ class HashLink extends Target {
 
 		// WINDOWS
 		if(Sys.systemName() == 'Windows') {
-			createPackage(hxmlContent, outputDir + '/hl_win/${projName}/', winFiles);
+			var packageDir = '$outputDir/hl_win/$projName';
+
+			createPackage(hxmlContent, packageDir, winFiles);
 
 			Term.print('Setting exe icon...');
-			runTool('rcedit.exe', [outputDir + '/hl_win/$projName/${projName}.exe', '--set-icon "${projDir}/${projName}.ico"']);
+			runTool('rcedit.exe', ['$packageDir/$projName.exe', '--set-icon "$projDir/$projName.ico"']);
 
 			// for steam, grab the appid
-			if(hxmlRequiresLib(hxmlContent, 'hlsteam') && FileSystem.exists(projDir + '/steam_appid.txt')) {
-				FileUtil.copyFile(projDir + '/steam_appid.txt', '$outputDir/hl_win/$projName/steam_appid.txt');
+			if(hxmlRequiresLib(hxmlContent, 'hlsteam') && FileSystem.exists('$projDir/steam_appid.txt')) {
+				FileUtil.copyFile('$projDir/steam_appid.txt', '$packageDir/steam_appid.txt');
 			}
 
-			FileUtil.zipFolder(outputDir + '/${projName}_hl_win_itch.zip', '$outputDir/hl_win/');
-			FileUtil.zipFolder(outputDir + '/${projName}_hl_win_steam.zip', '$outputDir/hl_win/$projName/');
+			FileUtil.zipFolder('$outputDir/${projName}_hl_win_itch.zip', '$outputDir/hl_win/');
+			FileUtil.zipFolder('$outputDir/${projName}_hl_win_steam.zip', '$packageDir/');
 		} 
 		// LINUX
 		else if(Sys.systemName() == 'Linux') {
-			createPackage(hxmlContent, outputDir + '/hl_linux/${projName}/', linuxFiles);
+			var packageDir = '$outputDir/hl_linux/$projName';
+
+			createPackage(hxmlContent, packageDir, linuxFiles);
 			
 			Term.print("Updating the linux files with execute permissions...");
-			Sys.command('chmod', ['+x', outputDir + '/hl_linux/$projName/$projName.x64']);
-			Sys.command('chmod', ['+x', outputDir + '/hl_linux/$projName/run.sh']);
+			Sys.command('chmod', ['+x', '$packageDir/$projName.x64']);
+			Sys.command('chmod', ['+x', '$packageDir/run.sh']);
 
 			// for steam, grab the appid
-			if(hxmlRequiresLib(hxmlContent, 'hlsteam') && FileSystem.exists(projDir + '/steam_appid.txt')) {
-				FileUtil.copyFile(projDir + '/steam_appid.txt', '$outputDir/hl_linux/$projName/steam_appid.txt');
+			if(hxmlRequiresLib(hxmlContent, 'hlsteam') && FileSystem.exists('$projDir/steam_appid.txt')) {
+				FileUtil.copyFile('$projDir/steam_appid.txt', '$packageDir/steam_appid.txt');
 			}
 
-			FileUtil.zipFolder(outputDir + '/${projName}_hl_linux_itch.zip', '$outputDir/hl_linux/');
-			FileUtil.zipFolder(outputDir + '/${projName}_hl_linux_steam.zip', '$outputDir/hl_linux/$projName/');
+			FileUtil.zipFolder('$outputDir/${projName}_hl_linux_itch.zip', '$outputDir/hl_linux/');
+			FileUtil.zipFolder('$outputDir/${projName}_hl_linux_steam.zip', '$outputDir/hl_linux/$projName/');
 		} 
 		// MAC
 		else if(Sys.systemName() == 'Mac') {
-			var outputFolder = '$outputDir/hl_mac/$projName';
-			var appFolder = '$outputFolder/$projName.app';
-			var exeFolder = '$appFolder/Contents/MacOS';
+			var packageDir = '$outputDir/hl_mac/$projName';
 
-			createPackage(hxmlContent, appFolder, macFiles);
+			createPackage(hxmlContent, packageDir, macFiles);
 			
 			Term.print("Updating the mac files with execute permissions...");
-			Sys.command('chmod', ['+x', '$exeFolder/$projName']);
+			Sys.command('chmod', ['+x', '$packageDir/$projName']);
 			
 			// for steam, grab the appid
 			if(hxmlRequiresLib(hxmlContent, 'hlsteam') && FileSystem.exists('$projDir/steam_appid.txt')) {
-				FileUtil.copyFile('$projDir/steam_appid.txt', '$exeFolder/steam_appid.txt');
+				FileUtil.copyFile('$projDir/steam_appid.txt', '$packageDir/steam_appid.txt');
 			}
 			
-			Term.print("Input the Developer ID (Developer ID Application: Simon Smith (TK421)):");
-			var appId:String = Sys.stdin().readLine();
+			FileUtil.zipFolder(outputDir + '/${projName}_hl_mac_itch.zip', '$outputDir/hl_mac/');
+			FileUtil.zipFolder(outputDir + '/${projName}_hl_mac_steam.zip', '$outputDir/hl_mac/$projName/');
+			
+			// Term.print("Input the Developer ID (Developer ID Application: Simon Smith (TK421)):");
+			// var appId:String = Sys.stdin().readLine();
 
-			Term.print("Codesigning the app bundle...");
-			Sys.command('codesign', ['-s', appId, '--timestamp', '--options', 'runtime', '-f', '--entitlements', '$outputFolder/entitlements.plist', '--deep', '$outputFolder/$projName.app']);
+			// Term.print("Codesigning the app bundle...");
+			// Sys.command('codesign', ['-s', appId, '--timestamp', '--options', 'runtime', '-f', '--entitlements', '$outputFolder/entitlements.plist', '--deep', '$outputFolder/$projName.app']);
 			
-			Term.print("Zipping the signed bundle...");
-			Sys.command('ditto', ['-c', '-k' ,'--keepParent', '$outputFolder/$projName.app', '$outputFolder/$projName.zip']);
+			// Term.print("Zipping the signed bundle...");
+			// Sys.command('ditto', ['-c', '-k' ,'--keepParent', '$outputFolder/$projName.app', '$outputFolder/$projName.zip']);
 			
-			Term.print("Enter notarytool profile name:");
-			var notaryProfile:String = Sys.stdin().readLine();
+			// Term.print("Enter notarytool profile name:");
+			// var notaryProfile:String = Sys.stdin().readLine();
 
-			Term.print("Notarizing app (might take a while)...");
-			Sys.command('xcrun notarytool', ['submit', '$outputFolder/$projName.zip', '--keychain-profile', notaryProfile, '--wait']);
+			// Term.print("Notarizing app (might take a while)...");
+			// Sys.command('xcrun notarytool', ['submit', '$outputFolder/$projName.zip', '--keychain-profile', notaryProfile, '--wait']);
 			
-			Term.print("Stapling notary to app...");
-			Sys.command('xcrun stapler', ['staple', '$outputFolder/$projName.app']);
-			// xcrun stapler staple "target/mac/Espanso.app"
-			// FileUtil.zipFolder(outputDir + '/${projName}_hl_mac_itch.zip', '$outputDir/hl_mac/');
-			// FileUtil.zipFolder(outputDir + '/${projName}_hl_mac_steam.zip', '$outputDir/hl_mac/$projName/');
+			// Term.print("Stapling notary to app...");
+			// Sys.command('xcrun stapler', ['staple', '$outputFolder/$projName.app']);
+
 		}
 	}
 	
@@ -106,18 +109,17 @@ class HashLink extends Target {
 		}
 		// MAC
 		else if(Sys.systemName() == 'Mac') {
-			FileUtil.createDirectory('$packageDir/Contents/Resources/');
-			FileUtil.copyFile(out, '$packageDir/Contents/MacOS/hlboot.dat');
+			FileUtil.copyFile(out, '$packageDir/hlboot.dat');
 			
-			Term.print("Updating Info.plist with project variables...");
-			var infoData = sys.io.File.getContent('$packageDir/Contents/Info.plist');
-			infoData = StringTools.replace(infoData, "$PROJ_NAME", projName);
-			sys.io.File.saveContent('$packageDir/Contents/Info.plist', infoData);
+			// Term.print("Updating Info.plist with project variables...");
+			// var infoData = sys.io.File.getContent('$packageDir/Contents/Info.plist');
+			// infoData = StringTools.replace(infoData, "$PROJ_NAME", projName);
+			// sys.io.File.saveContent('$packageDir/Contents/Info.plist', infoData);
 			
-			Term.print("Setting the icon for the app bundle...");
-			if(sys.FileSystem.exists('${projDir}/${projName}.icns')) {
-				FileUtil.copyFile('$projDir/$projName.icns', '$packageDir/Contents/Resources/$projName.icns');
-			}
+			// Term.print("Setting the icon for the app bundle...");
+			// if(sys.FileSystem.exists('${projDir}/${projName}.icns')) {
+			// 	FileUtil.copyFile('$projDir/$projName.icns', '$packageDir/Contents/Resources/$projName.icns');
+			// }
 		}
 	}
 
@@ -164,36 +166,36 @@ class HashLink extends Target {
 		platform: Mac,
         dir: 'dist_files/hl_mac/',
 		files: [
-			{ f:"hl", format:"$", d: "Contents/MacOS/"},
-			{ f:"Info.plist", d: "Contents/" },
-			{ f:"libhl.dylib", d: "Contents/MacOS/" },
-			{ f:"mysql.hdll", d: "Contents/MacOS/" },
+			{ f:"hl", format:"$" },
+			{ f:"libhl.dylib" },
+			{ f:"mysql.hdll" },
 			{ f:"entitlements.plist", d: "../" },
+			// { f:"Info.plist", d: "Contents/" },
 
-			{ f:"fmt.hdll", d: "Contents/MacOS/" },
-			{ f:"libpng16.16.dylib", d: "Contents/MacOS/" },
-			{ f:"libvorbis.0.dylib", d: "Contents/MacOS/" },
-			{ f:"libogg.0.dylib", d: "Contents/MacOS/" },
-			{ f:"libvorbisfile.3.dylib", d: "Contents/MacOS/" },
-			{ f:"libturbojpeg.0.dylib", d: "Contents/MacOS/" },
-			{ f:"libz.1.dylib", d: "Contents/MacOS/" },
+			{ f:"fmt.hdll" },
+			{ f:"libpng16.16.dylib" },
+			{ f:"libvorbis.0.dylib" },
+			{ f:"libogg.0.dylib" },
+			{ f:"libvorbisfile.3.dylib" },
+			{ f:"libturbojpeg.0.dylib" },
+			{ f:"libz.1.dylib" },
 
-			{ f:"ssl.hdll", d: "Contents/MacOS/" },
-			{ f:"libmbedtls.14.dylib", d: "Contents/MacOS/" },
-			{ f:"libmbedcrypto.7.dylib", d: "Contents/MacOS/" },
-			{ f:"libmbedx509.1.dylib", d: "Contents/MacOS/" },
+			{ f:"ssl.hdll" },
+			{ f:"libmbedtls.14.dylib" },
+			{ f:"libmbedcrypto.7.dylib" },
+			{ f:"libmbedx509.1.dylib" },
 
-			{ lib:"hlsdl", f:"sdl.hdll", d: "Contents/MacOS/" },
-			{ lib:"hlsdl", f:"libSDL2-2.0.0.dylib", d: "Contents/MacOS/" },
+			{ lib:"hlsdl", f:"sdl.hdll" },
+			{ lib:"hlsdl", f:"libSDL2-2.0.0.dylib" },
 			
-			{ lib:"heaps", f:"ui.hdll", d: "Contents/MacOS/" },
-			{ lib:"heaps", f:"uv.hdll", d: "Contents/MacOS/" },
-			{ lib:"heaps", f:"libuv.1.dylib", d: "Contents/MacOS/" },
-			{ lib:"heaps", f:"openal.hdll", d: "Contents/MacOS/" },
-			{ lib:"heaps", f:"libopenal.1.dylib", d: "Contents/MacOS/" },
+			{ lib:"heaps", f:"ui.hdll" },
+			{ lib:"heaps", f:"uv.hdll" },
+			{ lib:"heaps", f:"libuv.1.dylib" },
+			{ lib:"heaps", f:"openal.hdll" },
+			{ lib:"heaps", f:"libopenal.1.dylib" },
 
-			{ lib:"hlsteam", f:"steam.hdll", d: "Contents/MacOS/" },
-			{ lib:"hlsteam", f:"libsteam_api.dylib", d: "Contents/MacOS/" },
+			{ lib:"hlsteam", f:"steam.hdll" },
+			{ lib:"hlsteam", f:"libsteam_api.dylib" },
 		],
 	}
 
